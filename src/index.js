616cbfc54240eb58f5969b14efeec40f9e8b15e1
Carlos Roberto Gomes Junior <carlos.rberto@gmail.com>
2018-02-18 17:49:07 -0300

    fix abi path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3');
 var fs = require('fs');
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var abi = JSON.parse(fs.readFileSync('./build/contracts/GetSet.abi', 'utf8'));
+var abi = JSON.parse(fs.readFileSync('../build/contracts/GetSet.abi', 'utf8'));
 
 web3.eth.defaultAccount = '0x1a819cf8c813697f8480319e844ebcc272d82592';
 
